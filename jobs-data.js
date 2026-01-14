09fb6a04f2fe4c6efc918db6635621197f436844
ahbeefive <ahbeefive@gmail.com>
2026-01-14 10:55:00 +0700

    Update jobs-data.js

--- jobs-data.js
+++ jobs-data.js
@@ -1,6 +1,6 @@
 // Jobs Database - Update this file to change jobs for all visitors
 // This file is loaded by both the public site and admin panel
-// Last updated: 1/14/2026, 10:48:53 AM
+// Last updated: 1/14/2026, 10:54:44 AM
 
 window.JOBS_DATABASE = [
     {
@@ -35,6 +35,17 @@ window.JOBS_DATABASE = [
         "image": "https://via.placeholder.com/400x200/ec4899/ffffff?text=UI%2FUX+Designer",
         "createdAt": 1768362389703,
         "order": 3
+    },
+    {
+        "id": 1768362744916.6643,
+        "order": 4,
+        "title": "Test",
+        "salary": "dwdw",
+        "location": "New York, NY",
+        "requirements": "dwdwdwdw",
+        "applyLink": "https://t.me/CarloBinze",
+        "image": "https://res.cloudinary.com/dvwilkvh8/image/upload/v1768209220/job-portal/zwnwjnz7djennvu8iznj.png",
+        "createdAt": 1768362744916
     }
 ];
 
